b762e082fafe0610051eff75fa7e7bbdd486727a
yuua <mumin.nonnon0216@gmail.com>
2016-11-17 14:40:31 +0900

    引数取ってsearchする

--- ruby/itunes/serach.rb
+++ ruby/itunes/serach.rb
@@ -1,18 +1,20 @@
 #!/usr/bin/env ruby
 require 'itunes-search-api'
-require 'mongo'
-require './config/config'
+# require 'hashie'
 
-connect = Mongo::Connection.new(MONGO_CONNECT)
-db = connect.db(DB)
-collect = db.collection(COLLECTION)
+# require 'mongo'
+# require './config/config'
+
+# connect = Mongo::Connection.new(MONGO_CONNECT)
+# db = connect.db(DB)
+word = ARGV[0]
 
 ITunesSearchAPI.search(
-  term: "石原さとみ",
+  term: "#{word}",
   country: "jp",
   media: "movie",
   lang: "ja_jp",
-  limit: "500"
+#   limit: "100"
 ).each do |item|
-  collect.insert(item)
+  puts "#{word} #{item["artistName"]} #{item["trackName"].gsub(/\s/,"")} #{item["trackCensoredName"].gsub(/\s/,"")} #{item["artworkUrl60"]} #{item["longDescription"].gsub(/\s/,"")}"
 end
